398e9a8de430998d14a73af045f5f5a34bb68c72
kabeer1gujral <kabeergujraal@gmail.com>
2024-06-22 12:07:39 +0530

    Update Number_guessing_game.py

--- Number_guessing_game.py
+++ Number_guessing_game.py
@@ -1,7 +1,7 @@
 import random
 def Number_guessing_game(a, b):
     r1 = random.randint(a, b)
-    r2 = int(input("Guess a random number between {} and {}: ".format(a, b))
+    r2 = int(input("Guess a random number between {} and {}: ".format(a, b)))
     if r2 == r1:
         print("Well done! You guessed right.")
     elif r2 > r1:
