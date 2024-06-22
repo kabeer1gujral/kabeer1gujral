06f2c637ab126c33eae4356662c3c8c5a04198f0
kabeer1gujral <kabeergujraal@gmail.com>
2024-06-22 16:15:10 +0530

    Update Number_guessing_game.py

--- Number_guessing_game.py
+++ Number_guessing_game.py
@@ -1,10 +1,20 @@
 import random
 def Number_guessing_game(a, b):
     r1 = random.randint(a, b)
-    r2 = int(input("Guess a random number between {} and {}: ".format(a, b)))
-    if r2 == r1:
-        print("Well done! You guessed right.")
-    elif r2 > r1:
-        print("You guessed too high!")
+    while True:
+        r2 = int(input("Guess a random number between {} and {}: ".format(a, b)))
+        if r2 == r1:
+            print("Well done! You guessed right.")
+            break
+        elif r2 > r1:
+            print("You guessed too high!")
+        else:
+            print("You guessed too low!")
+    play_again = input("Do you want to play again? (yes/no): ")
+    if play_again.lower() == 'yes':
+        Number_guessing_game()
     else:
-        print("You guessed too low!")
+        print("Thank you for playing!")
+
+if __name__ == "__main__":
+    guess_number()
