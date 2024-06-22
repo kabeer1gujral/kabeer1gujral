ad28062078cbf0d58b3de844cc59fbfd53845555
kabeer1gujral <kabeergujraal@gmail.com>
2024-06-22 11:59:58 +0530

    Create Number_guessing_game.py
    number guessing game done in python

---
+++ Number_guessing_game.py
@@ -0,0 +1,10 @@
+import random
+def Number_guessing_game(a, b):
+    r1 = random.randint(a, b)
+    r2 = int(input("Guess a random number between {} and {}: ".format(a, b))
+    if r2 == r1:
+        print("Well done! You guessed right.")
+    elif r2 > r1:
+        print("You guessed too high!")
+    else:
+        print("You guessed too low!")
